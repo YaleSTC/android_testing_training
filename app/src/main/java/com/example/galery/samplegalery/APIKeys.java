45229449dcf3e368d91b65c58f3c722df3cc2c80
Stan <stanislaw.swidwinski@yale.edu>
2016-12-22 09:51:00 -0500

    API keys class.

---
+++ app/src/main/java/com/example/galery/samplegalery/APIKeys.java
@@ -0,0 +1,14 @@
+package com.example.galery.samplegalery;
+
+/**
+ * Created by Stan on 12/22/16.
+ */
+
+
+/*
+ * Please replace this with a valid API key. The key underneath is a fake - remember, never share
+ * keys publically!
+ */
+public class APIKeys {
+    public static final String FLICKR_API_KEY = "64ffdc1fd4b18211f4f98513848d2335";
+}
