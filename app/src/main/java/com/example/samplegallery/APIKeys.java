f20a08d31f2fc81548e860293c96240974f817e4
Parthib Samadder <Parthib Samadder>
2017-01-22 14:23:30 -0500

    Finished test

--- app/src/main/java/com/example/samplegallery/APIKeys.java
+++ app/src/main/java/com/example/samplegallery/APIKeys.java
@@ -10,5 +10,5 @@ package com.example.samplegallery;
  * keys publically!
  */
 public class APIKeys {
-    public static final String FLICKR_API_KEY = "64ffdc1fd4b18211f4f98513848d2335";
+    public static final String FLICKR_API_KEY = "3cc1361b2cf4ea990e81990559f46567";
 }
