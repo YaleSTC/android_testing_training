0c985ccf31cca3a989bce5c32320d3f6cafe093d
lasyasreepada <lasya.sreepada@gmail.com>
2017-01-28 13:54:44 -0500

    Wrote a JUnit test for the getCacheSize() method by mocking DisplayMetrics and Context objects. Test failed, reviewed the code in LruBitmapCache and changed the number of screens worth of images stored by the app from 4 to 3. Tests now work Addresses Part 1 of the Testing unit in android_testing_training

---
+++ app/src/test/java/com/example/samplegallery/Utilities/LruBitmapCacheTest.java
@@ -0,0 +1,35 @@
+package com.example.samplegallery.Utilities;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.runners.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LruBitmapCacheTest {
+
+    @Mock
+    Context mMockContext;
+
+    @Test
+    public void getCacheSizeTest() throws Exception {
+        //given a mocked object injected into the object under test
+        mMockContext = Mockito.mock(Context.class, Mockito.RETURNS_DEEP_STUBS);
+
+        //create a mock display metrics object
+        DisplayMetrics mMockDisplayMetrics = new DisplayMetrics();
+        mMockDisplayMetrics.widthPixels = 6;
+        mMockDisplayMetrics.heightPixels = 10;
+
+        //mock the chained methods
+        when(mMockContext.getResources().getDisplayMetrics())
+                .thenReturn(mMockDisplayMetrics);
+
+        assertEquals(LruBitmapCache.getCacheSize(mMockContext), 6 * 10 * 3 * 4);
+    }
+}
